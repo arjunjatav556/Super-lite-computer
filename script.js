e309f5ec5b8440558505199d2b13734b6d02dae5
arjunjatav556 <arjun1292008@gmail.com>
2025-10-14 22:18:44 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,10 +5,10 @@ new QRCode(document.getElementById("qrcode"), {
 });
 
 const sampleData = [
-  {roll: 'SLTC-101', year: '2025', name: 'Ramesh Kumar', father: 'Sh. Mohan Lal', mother: 'Smt. Rekha Devi',
+  {roll: 'SLTC-101', year: '2025', name: 'Arjun', father: 'Sh. Madanpal', mother: 'Smt. Lata',
     subjects: [
       {name:'Computer Fundamentals', theory:78, practical:12, theoryMax:80, practicalMax:20},
-      {name:'MS Office', theory:85, practical:10, theoryMax:80, practicalMax:20}
+      {name:'MS Office', theory:90, practical:45, theoryMax:100, practicalMax:50}
     ]
   },
   {roll: 'SLCTC-102', year: '2025', name: 'Sita Verma', father: 'Sh. Rajesh Verma', mother: 'Smt. Meena Verma',
